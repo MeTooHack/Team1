a51c97ce149629b61456beb441fc277542e14660
Oskar Nyberg <oskar@oskarnyberg.com>
2018-02-03 16:14:46 +0100

    Workin'

--- js/index.js
+++ js/index.js
@@ -1,29 +1,76 @@
-const Web3 = require('web3')
 const nacl = require('tweetnacl')
 const Identify = require('./identify.js')
 nacl.util = require('tweetnacl-util')
+const sha3 = require('solidity-sha3').default
+const bn = require('bignumber.js')
+const enc = require('./enc')
+const Promise = require('bluebird')
 
-const abi = [{"constant":true,"inputs":[],"name":"getMyRegistrations","outputs":[{"name":"","type":"bytes32[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"revealer","type":"address"}],"name":"getReveal","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"hash","type":"bytes32"}],"name":"getRegistered","outputs":[{"components":[{"name":"sender","type":"address"},{"name":"pubkey","type":"bytes32"}],"name":"","type":"tuple[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"hash","type":"bytes32"},{"name":"pubkey","type":"bytes32"}],"name":"Register","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"ipfs","type":"string"}],"name":"Reveal","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}];
+const abi = [{"constant":true,"inputs":[],"name":"getMyRegistrations","outputs":[{"name":"","type":"bytes32[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"revealer","type":"address"}],"name":"getReveal","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"ipfs","type":"string"}],"name":"reveal","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"hash","type":"bytes32"},{"name":"pubkey","type":"string"},{"name":"sig1","type":"string"},{"name":"sig2","type":"string"}],"name":"register","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"hash","type":"bytes32"},{"name":"i","type":"uint256"}],"name":"getRegisteredSender","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"hash","type":"bytes32"},{"name":"i","type":"uint256"}],"name":"getRegisteredSig1","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"hash","type":"bytes32"}],"name":"getRegisteredLength","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"hash","type":"bytes32"},{"name":"i","type":"uint256"}],"name":"getRegisteredSig2","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"hash","type":"bytes32"},{"name":"i","type":"uint256"}],"name":"getRegisteredPubkey","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"}];
 
 let contract
+let keys = {
+  publicKey: "mDdaQrp0W2NuAunDGWH89PeBQy8i/5iTeklp8bU4MlQ=",
+  secretKey: "aciCI2usRhyL3mlUyKGyXeDQy4qMSqybfYqebcGAuB8="
+}
+let address
 
 window.addEventListener('load', function() {
-  web3 = new Web3(Web3.givenProvider)
   initContract()
   showRegistrations()
 })
 
 function initContract(){
-  contract = new web3.eth.Contract(abi, '0x3d7e4728de4ee818575c61c75963d2625f47fc99');
+  contract = web3.eth.contract(abi).at('0xc89095ac1c852abbd0ee6b8ec43e0c292b95878f')
+  contract = Promise.promisifyAll(contract)
 }
 
-function register(el) {
+window.connect = function() {
+  web3.eth.getCoinbase((error, result) => {
+    address = result
+    document.getElementById('address').innerHTML = address
+  })
+}
 
+window.register = function(form) {
+  let hash = sha3(form.name.value)
+  contract.register(hash, keys.publicKey, "", "", error => error && console.log(error))
+
+  return false
+}
+
+async function reveal(hash) {
+  let encryptedList = []
+  let result = await contract.getRegisteredLengthAsync(hash)
+  let length = new bn(result).toNumber()
+  for (let i = 0; i < length; i++) {
+    let toPubKey = await contract.getRegisteredPubkeyAsync(hash, i)
+    let encrypted = enc.encrypt(toPubKey, keys.publicKey, keys.secretKey, "email@example.com")
+    encryptedList.push(encrypted)
+  }
+
+  let string = JSON.stringify(encryptedList)
+  contract.revealAsync(string)
 }
 
 function showRegistrations() {
-  contract.methods.getMyRegistrations.call((error, result) => {
-    document.getElementById('myRegistrations').innerHTML = result
+  contract.getMyRegistrations.call((error, registrations) => {
+    registrations = [...new Set(registrations)]
+    registrations.forEach(r => {
+      contract.getRegisteredLength(r, (error, result) => {
+        let li = document.createElement('li')
+        let n = new bn(result).toNumber()
+        let r_short = r.substr(0, 12) + "..."
+        li.innerHTML = r_short + " - " + n
+
+        let reveal_button = document.createElement('button')
+        reveal_button.onclick = reveal.bind(this, r)
+        reveal_button.textContent = "Reveal"
+        
+        li.appendChild(reveal_button)
+        document.getElementById('myRegistrations').appendChild(li)
+      })
+    })
   })
 }
 
